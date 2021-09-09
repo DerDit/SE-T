684ec23d3f39a76aff0ff7d22484925c4948d3e3
DerDit <deron.ditiecher@capgemini.com>
2021-09-09 16:57:50 +0200

    exercise 6 Scanner

--- SE-Track/src/entity/scanner.java
+++ SE-Track/src/entity/scanner.java
@@ -5,13 +5,27 @@ import  java.util.Scanner;
 public class scanner {
     public static void main(String[] args) {
 
+       //Scanner aanmaken
        Scanner input = new Scanner(System.in);
+
+       //tekst bij de scanner
        System.out.print("Add review:");
 
+       //input van de scanner
        String value =input.nextLine();
+
+       //output van de scanner
        System.out.println("Review is: " + value);
 
-       input.close();
+       //scanner 2.0 toevoegen
+        Scanner input2 = new Scanner(System.in);
+        System.out.print("Add dj:");
+
+        String value2 =input2.nextLine();
+        System.out.println("dj heet: " + value2);
+
+        //afsluiten van de input
+        input.close();
     }
 
 }
