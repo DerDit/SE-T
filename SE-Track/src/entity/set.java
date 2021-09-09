60c0bc47d2df8b90fa9047883db0a6dac22d54fd
DerDit <deron.ditiecher@capgemini.com>
2021-09-09 15:45:31 +0200

    exercise 5 entitiy

---
+++ SE-Track/src/entity/set.java
@@ -0,0 +1,61 @@
+package entity;
+
+public class set implements java.io.Serializable{
+    private Long id;
+    private String name;
+    private String day;
+    private String stage;
+    private String begintime;
+    private String endtime;
+
+    public set() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public String getStage() {
+        return stage;
+    }
+
+    public void setStage(String stage) {
+        this.stage = stage;
+    }
+
+    public String getBegintime() {
+        return begintime;
+    }
+
+    public void setBegintime(String begintime) {
+        this.begintime = begintime;
+    }
+
+    public String getEndtime() {
+        return endtime;
+    }
+
+    public void setEndtime(String endtime) {
+        this.endtime = endtime;
+    }
+}
