438b2bdccd16f417116a4a1d569646149cc5cabc
DerDit <deron.ditiecher@capgemini.com>
2021-09-09 15:44:10 +0200

    exercise 5 entitiy

--- SE-Track/src/entity/review.java
+++ SE-Track/src/entity/review.java
@@ -1,4 +1,36 @@
 package entity;
 
-public class review {
+import java.io.Serializable;
+
+public class review implements java.io.Serializable {
+    private Long id;
+    private int rating;
+    private String comment;
+
+    public review() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 }
