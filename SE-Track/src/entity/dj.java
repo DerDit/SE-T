7f464f893010a5dbb8d7822841ce0717e196085a
DerDit <deron.ditiecher@capgemini.com>
2021-09-09 15:42:03 +0200

    exercise 5 entitiy

---
+++ SE-Track/src/entity/dj.java
@@ -0,0 +1,45 @@
+package entity;
+
+import java.io.Serializable;
+
+public class dj implements java.io.Serializable {
+    private Long id;
+    private String name;
+    private int age;
+    private String style;
+
+    public dj(){
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public void setStyle(String style) {
+        this.style = style;
+    }
+}
